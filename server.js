45080e01c784132a84d48da72ec37a90aaf5c73f
SachinRuhela1922 <156918990+SachinRuhela1922@users.noreply.github.com>
2024-12-05 06:29:58 -0800

    Update server.js

--- server.js
+++ server.js
@@ -226,6 +226,10 @@ app.post('/login', async (req, res) => {
 
 
 
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 
 // Start the server
